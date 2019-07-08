fbbdac846498ea998897a975c2396c2f4fef1a96
Divvi Naga Venkata Satish <divvinagavenkata.satish69@gmail.com>
2019-07-07 21:18:56 -0700

    Declare function createTrafficObjects_Paris

--- src/TrafficSimulator-L1.cpp
+++ src/TrafficSimulator-L1.cpp
@@ -7,6 +7,14 @@
 #include "Intersection.h"
 #include "Graphics.h"
 
+
+// Paris
+
+void createTrafficObjects_Paris(std::vector<std::shared_ptr<Street>> &streets, std::vector<std::shared_ptr<Intersection>> &intersections, std::vector<std::shared_ptr<Vehicle>> &vehicles, std::string &filename, int nVehicles)
+{
+    
+}
+
 // NYC
 void createTrafficObjects(std::vector<std::shared_ptr<Street>> &streets, std::vector<std::shared_ptr<Intersection>> &intersections, std::vector<std::shared_ptr<Vehicle>> &vehicles, std::string &filename, int nVehicles)
 {
@@ -14,51 +22,47 @@ void createTrafficObjects(std::vector<std::shared_ptr<Street>> &streets, std::ve
     filename = "../data/nyc.jpg";
 
     // init traffic objects
-    int nIntersections = 9;
+    int nIntersections = 6;
     for (size_t ni = 0; ni < nIntersections; ni++)
     {
         intersections.push_back(std::make_shared<Intersection>());
     }
 
     // position intersections in pixel coordinates
-    intersections.at(0)->setPosition(385, 270);
-    intersections.at(1)->setPosition(1240, 80);
-    intersections.at(2)->setPosition(1625, 75);
-    intersections.at(3)->setPosition(2110, 75);
-    intersections.at(4)->setPosition(2840, 175);
-    intersections.at(5)->setPosition(3070, 680);
-    intersections.at(6)->setPosition(2800, 1400);
-    intersections.at(7)->setPosition(400, 1100);
-    intersections.at(8)->setPosition(1700, 900);
+    intersections.at(0)->setPosition(1430, 625);
+    intersections.at(1)->setPosition(2575, 1260);
+    intersections.at(2)->setPosition(2200, 1950);
+    intersections.at(3)->setPosition(1000, 1350);
+    intersections.at(4)->setPosition(400, 1000);
+    intersections.at(5)->setPosition(750, 250);
+
     // create streets and connect traffic objects
-    int nStreets = 8;
+    int nStreets = 7;
     for (size_t ns = 0; ns < nStreets; ns++)
     {
         streets.push_back(std::make_shared<Street>());
-        streets.at(ns)->setInIntersection(intersections.at(ns));
-        streets.at(ns)->setOutIntersection(intersections.at(8));
     }
 
-    // streets.at(0)->setInIntersection(intersections.at(0));
-    // streets.at(0)->setOutIntersection(intersections.at(1));
+    streets.at(0)->setInIntersection(intersections.at(0));
+    streets.at(0)->setOutIntersection(intersections.at(1));
 
-    // streets.at(1)->setInIntersection(intersections.at(1));
-    // streets.at(1)->setOutIntersection(intersections.at(2));
+    streets.at(1)->setInIntersection(intersections.at(1));
+    streets.at(1)->setOutIntersection(intersections.at(2));
 
-    // streets.at(2)->setInIntersection(intersections.at(2));
-    // streets.at(2)->setOutIntersection(intersections.at(3));
+    streets.at(2)->setInIntersection(intersections.at(2));
+    streets.at(2)->setOutIntersection(intersections.at(3));
 
-    // streets.at(3)->setInIntersection(intersections.at(3));
-    // streets.at(3)->setOutIntersection(intersections.at(4));
+    streets.at(3)->setInIntersection(intersections.at(3));
+    streets.at(3)->setOutIntersection(intersections.at(4));
 
-    // streets.at(4)->setInIntersection(intersections.at(4));
-    // streets.at(4)->setOutIntersection(intersections.at(5));
+    streets.at(4)->setInIntersection(intersections.at(4));
+    streets.at(4)->setOutIntersection(intersections.at(5));
 
-    // streets.at(5)->setInIntersection(intersections.at(5));
-    // streets.at(5)->setOutIntersection(intersections.at(0));
+    streets.at(5)->setInIntersection(intersections.at(5));
+    streets.at(5)->setOutIntersection(intersections.at(0));
 
-    // streets.at(6)->setInIntersection(intersections.at(0));
-    // streets.at(6)->setOutIntersection(intersections.at(3));
+    streets.at(6)->setInIntersection(intersections.at(0));
+    streets.at(6)->setOutIntersection(intersections.at(3));
 
     // add vehicles to streets
     for (size_t nv = 0; nv < nVehicles; nv++)
